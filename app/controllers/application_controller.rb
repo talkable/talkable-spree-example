87d99c30e49ad15d486baedd7d3cea217a570217
vitalikdanchenko <devyjones@gmail.com>
2016-08-25 16:04:54 +0300

    add standalone campaign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :load_talkable_offer
+  before_action :inject_talkable_offer
+
+  protected
+
+  def load_talkable_offer
+    origin = Talkable.register_affiliate_member(
+      email: current_spree_user&.email,
+      campaign_tags: (request.path == '/invite' ? 'invite' : 'popup'),
+    )
+    @offer ||= origin&.offer
+  end
+
+  def inject_talkable_offer
+    Deface::Override.new(
+      virtual_path: "spree/layouts/spree_application",
+      name: "talkable-offer",
+      insert_before: "div#content",
+      text: "<%= @offer&.advocate_share_iframe&.html_safe %>"
+    )
+  end
+
 end
